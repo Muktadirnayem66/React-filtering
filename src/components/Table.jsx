5bc2d5f755a8a5e7fafa4370345486cbc961ac9b
Md.Muktadir Nayem <mmuktadir629@gmail.com>
2023-07-14 16:22:03 +0600

    final commit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,17 +2,26 @@ import React, { useState } from "react"
 
 const Table= ({contacts})=>{
     const [filter, setFilter] = useState("all")
+    const [searchTerm, setSearchTerm] = useState("")
+
     const handleChange =(e)=>{
         setFilter(e.target.value)
     }
 
+    const search =  (contact)=>(
+      contact.name.includes(searchTerm) ||
+      contact.email.includes(searchTerm)
+    )
+
+
     let filteredContacts = []
     if(filter === "all"){
-        filteredContacts = contacts
+        filteredContacts = searchTerm ? contacts.filter(search) : contacts
     }else{
-      filteredContacts = contacts.filter((contact)=>contact.group === filter)
+      filteredContacts = contacts.filter((contact)=>contact.group === filter && search(contact))
     }
 
+    
     return(
         <>
         <div>
@@ -24,6 +33,10 @@ const Table= ({contacts})=>{
                 <option value="home">Home</option>
                 <option value="office">Office</option>
             </select>
+
+            <input type="text" value={searchTerm}
+            placeholder="Search..."
+            onChange={e=>setSearchTerm(e.target.value)} />
         </div>
       <table>
         <thead>
